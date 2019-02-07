a43aa350e30cb1941ac653492069dc0e4312d12c
Alvin Yang <alvin.yang73@gmail.com>
2019-02-06 17:53:20 -0800

    Added autorun generation functionality with variable step speed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Board } from './game/board';
+import Board from './game/board';
 import './App.css';
 
 class App extends Component {
--- src/game/board.js
+++ src/game/board.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ReactTimeout from 'react-timeout';
 import './css/board.css';
 import { Cell } from './cell';
 
-export class Board extends React.Component {
+class Board extends React.Component {
 
     constructor(props) {
         super(props);
+        
+        // Generate a new blank grid
         let cells = [];
         for (let i = 0; i < 40; i++) {
             let cellRow = [];
@@ -14,12 +17,16 @@ export class Board extends React.Component {
             }
             cells.push(cellRow);
         }
-        this.setState({grid : cells});
         this.state = {
-            grid : cells
+            grid : cells,
+            speed: 2,
+            running: false
         }
         this.nextGeneration = this.nextGeneration.bind(this);
         this.toggleCell = this.toggleCell.bind(this);
+        this.autoGeneration = this.autoGeneration.bind(this);
+        this.pauseGeneration = this.pauseGeneration.bind(this);
+        this.sliderChange = this.sliderChange.bind(this);
     }
     
     createTable = () => {
@@ -42,6 +49,7 @@ export class Board extends React.Component {
         return table;
     }
 
+    // Toggles a given cell between dead and alive
     toggleCell = (cell) => {
         let newGrid = this.state.grid;
         if (cell.alive) {
@@ -52,7 +60,9 @@ export class Board extends React.Component {
         this.setState({grid: newGrid});
     }
 
+    // Generates the next generation of the grid
     nextGeneration = (e) => {
+        // Create a new grid so that changed cells don't affect results for the current generation
         let newGrid = [];
         for (let i = 0; i < this.state.grid.length; i++) {
             let newGridRow = [];
@@ -64,11 +74,14 @@ export class Board extends React.Component {
         this.setState({grid : newGrid});
     }
 
+    // Checks the neighbors of a given cell's coordinates returning true if the given cell should be alive and false if it should be dead
     checkNeighbors = (row, col) => {
         let numAliveNeighbors = 0;
         for (let i = row - 1; i <= row + 1; i++) {
+            // Pass over row if out of bounds
             if (i >= 0 && i < this.state.grid.length) {
                 for (let j = col - 1; j <= col + 1; j++) {
+                    // Pass over column if out of bounds
                     if (j >= 0 && j < this.state.grid[i].length) {
                         if (this.state.grid[i][j]) {
                             numAliveNeighbors++;
@@ -92,15 +105,36 @@ export class Board extends React.Component {
         }
     }
 
+    autoGeneration = (e) => {
+        this.setState({running: true});
+        this.runInterval = this.props.setInterval(this.nextGeneration, 1000 / this.state.speed);
+    }
+
+    pauseGeneration = (e) => {
+        this.setState({running: false});
+        this.props.clearInterval(this.runInterval);
+    }
+
+    sliderChange = (e) => {
+        this.setState({speed: e.target.value});
+    }
+
     render() {
         return(
             <div>
                 <table className="boardTable">
                     {this.createTable()}
                 </table>
-                <button onClick={this.nextGeneration}>Step Generation</button>
+                <button onClick={this.nextGeneration} disabled={this.state.running}>Step Generation</button>
+                <div>
+                    <span>Auto-Run Speed (iterations per second): {this.state.speed}</span>
+                    <input id="stepSpeed" type="range" min="1" max="4" defaultValue={this.state.speed} onChange={this.sliderChange} step="1"/>
+                    <button onClick={this.autoGeneration} disabled={this.state.running}>Auto-Run Generation</button>
+                    <button onClick={this.pauseGeneration} disabled={!this.state.running}>Pause Generation</button>
+                </div>
             </div>
         );
     }
-
 }
+
+export default ReactTimeout(Board);
