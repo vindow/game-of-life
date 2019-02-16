a27e73a648e743c5759fb5191d5ec29b88efd6fe
Alvin Yang <alvin.yang73@gmail.com>
2019-02-15 16:41:28 -0800

    Updates

---
+++ precache-manifest.e5edd7e94bd3f680061f848ca2ffacdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b485c445b65ba023756b",
+    "url": "/game-of-life/static/js/runtime~main.b485c445.js"
+  },
+  {
+    "revision": "8a2a27d292d6e8926341",
+    "url": "/game-of-life/static/js/main.8a2a27d2.chunk.js"
+  },
+  {
+    "revision": "cc3ceb9db89f47b9e171",
+    "url": "/game-of-life/static/js/2.cc3ceb9d.chunk.js"
+  },
+  {
+    "revision": "8a2a27d292d6e8926341",
+    "url": "/game-of-life/static/css/main.9878d081.chunk.css"
+  },
+  {
+    "revision": "52da74297be3d230e0bfbb26455762fb",
+    "url": "/game-of-life/index.html"
+  }
+];
